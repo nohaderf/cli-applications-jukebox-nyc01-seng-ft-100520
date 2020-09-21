8042ea4daf84365d1acaf734ef18e055f45a8cb4
Freda Hon <freda.hon@gmail.com>
2020-09-21 23:56:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -53,7 +53,10 @@ end
 # If the user's response is a valid song number or song name, the method should puts out: "Playing <song name>"
 # Otherwise, it should puts out: "Invalid input, please try again"
 
-def
+def method(songs)
+  puts "Please enter a song name or number:"
+  
+end
 
 def exit_jukebox
   puts "Goodbye."

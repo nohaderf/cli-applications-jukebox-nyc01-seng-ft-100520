cb61af9f6c684f76b0029658b62da92c9683d04a
Freda Hon <freda.hon@gmail.com>
2020-09-21 23:28:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,6 +26,19 @@ def help
   puts "- exit : exits this program"
 end
 
+# THE LIST METHOD
+# This method takes in an argument of the songs array of puts out the following:
+
+  # 1. Phoenix - 1901
+  # 2. Tokyo Police Club - Wait Up
+  # 3. Sufjan Stevens - Too Much
+  # 4. The Naked and the Famous - Young Blood
+  # 5. (Far From) Home - Tiga
+  # 6. The Cults - Abducted
+  # 7. Phoenix - Consolation Prizes
+  # 8. Harry Chapin - Cats in the Cradle
+  # 9. Amos Lee - Keep It Loose, Keep It Tight
+
 def list(songs)
   songs.each_with_index do |song, index|
     puts "#{index+1}. #{song}"
